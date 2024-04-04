ed72f31b4de02a4238b5bd70a86d05f345b535a1
Artem Chuban <ArtemChuban@yandex.ru>
2024-04-04 19:18:33 +0200

    color fix

--- backend/app/tags/schemas.py
+++ backend/app/tags/schemas.py
@@ -1,7 +1,6 @@
 import uuid
 
 from pydantic import BaseModel, Field
-from pydantic_extra_types.color import Color
 
 from app.schemas import TagId
 
@@ -9,4 +8,4 @@ from app.schemas import TagId
 class Tag(BaseModel):
     id: TagId = Field(default_factory=lambda: TagId(str(uuid.uuid4())))
     name: str = Field(min_length=1)
-    color: Color
+    color: str
