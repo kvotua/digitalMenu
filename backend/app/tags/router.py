3c1835a2dc973b58fd6767027a9f59ca19baf439
Artem Chuban <ArtemChuban@yandex.ru>
2024-04-08 17:01:59 +0200

    admin checks

--- backend/app/tags/router.py
+++ backend/app/tags/router.py
@@ -1,40 +1,42 @@
 from typing import Annotated, Optional
 
-from fastapi import APIRouter, Body, Header, Path
+from fastapi import APIRouter, Body, Depends, Path
 from pydantic_extra_types.color import Color
 
-from app.schemas import JWToken, TagId
+from app.schemas import TagId
 from app.tags.schemas import Tag
 from app.tags.service import create_tag, delete, get_all, update
+from app.utils import check_admin
 
 router = APIRouter(prefix="/tags", tags=["Tags"])
 
 
 @router.get(
     "/",
-    description="Get all tags (now JWT verification now)",
+    description="Get all tags",
+    dependencies=[Depends(check_admin)],
 )
-async def get_all_tags(
-    token: Annotated[JWToken, Header()],
-) -> list[Tag]:
+async def get_all_tags() -> list[Tag]:
     return get_all()
 
 
 @router.post(
     "/",
-    description="Creates new tag (now JWT verification now)",
+    description="Creates new tag",
+    dependencies=[Depends(check_admin)],
 )
 async def post_tag(
-    token: Annotated[JWToken, Header()],
     name: Annotated[str, Body(min_length=1)],
     color: Annotated[Color, Body()],
 ) -> TagId:
     return create_tag(name, color)
 
 
-@router.patch("/{id}")
+@router.patch(
+    "/{id}",
+    dependencies=[Depends(check_admin)],
+)
 async def update_tag_info(
-    token: Annotated[JWToken, Header()],
     id: Annotated[TagId, Path()],
     name: Optional[Annotated[str, Body(min_length=1)]],
     color: Optional[Annotated[Color, Body()]],
@@ -44,10 +46,10 @@ async def update_tag_info(
 
 @router.delete(
     "/{id}",
-    description="Also remove it from all compositions (now JWT verification now)",
+    description="Also remove it from all compositions",
+    dependencies=[Depends(check_admin)],
 )
 async def delete_tag(
-    token: Annotated[JWToken, Header()],
     id: Annotated[TagId, Path()],
 ) -> None:
     delete(id)
